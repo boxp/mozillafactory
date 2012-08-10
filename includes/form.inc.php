5cf4fe5250201f52e8baf1f96a6963dd98a4d7f7
dynamis <dynamis@9c0e29c4-50a7-dd11-974c-001b24efe40c>
2012-08-10 05:45:56 +0000

    trunk->prod r3078:3130
    git-svn-id: file:///home/svn/repo/www/mozillafactory.org/tags/production@3131 9c0e29c4-50a7-dd11-974c-001b24efe40c

--- includes/form.inc.php
+++ includes/form.inc.php
@@ -1,28 +1,21 @@
 <?
-
-// デバッグフラグ
-$_debug = false;
-$_debug_mail = 'shuntaro@mozilla-japan.org';
-$_debug_mail = 'dynamis@mozilla-japan.org';
-
-// gettext
-setlocale(LC_ALL, $locale);
+@include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/common.inc.php';
 
 $_type = $_POST['type'] ? $_POST['type'] : $_GET['type'];
 $_isEvent = $_type == 'join-scp';
 
 // フォームのカスタマイズ
 $_form = (object) array(
-  'description' => 'Mozilla Factory の取り組みに興味のある方は、このフォームからご連絡ください。',
+  'description' => _('Mozilla Factory の取り組みに興味のある方は、このフォームからご連絡ください。'),
   'required' => array(),
   'default_comment' => ''
 );
 if (!empty($_type)) {
   switch ($_type) {
     case 'join-scp':
-      $_form->description = 'Mozilla Summer Code Party in Kobe への参加をご希望の方は、このフォームからお申込みください。';
+      $_form->description = _('Mozilla Summer Code Party in Kobe への参加をご希望の方は、このフォームからお申込みください。');
       $_form->required = array('age', 'comment');
-      $_form->default_comment = '来場予定時間:';
+      $_form->default_comment = _('来場予定時間:');
       break;
   }
 }
@@ -33,37 +26,66 @@ if (!empty($_POST['comment'])) {
 }
 
 // 件名
-$_subject = 'Mozilla Factory について';
+$_subject = _('Mozilla Factory について');
 if (!empty($_POST['subject'])) {
   $_subject = $_POST['subject'];
 } else if (!empty($_type)) {
   switch ($_type) {
     case 'projects':
-      $_subject = 'Mozilla Factory のプロジェクトについて';
+      $_subject = _('Mozilla Factory のプロジェクトについて');
       break;
     case 'webmaker':
-      $_subject = 'Webmaker について';
+      $_subject = _('Webmaker について');
       break;
     case 'join-webmaker':
-      $_subject = 'Webmaker プロジェクト参加希望';
+      $_subject = _('Webmaker プロジェクト参加希望');
       break;
     case 'mecha':
-      $_subject = 'Mecha-Mozilla について';
+      $_subject = _('Mecha-Mozilla について');
       break;
     case 'join-mecha':
-      $_subject = 'Mecha-Mozilla プロジェクト参加希望';
+      $_subject = _('Mecha-Mozilla プロジェクト参加希望');
       break;
     case 'mentor':
-      $_subject = 'メンター参加希望';
+      $_subject = _('メンター参加希望');
       break;
     case 'join-buttobi':
-      $_subject = 'ぶっとびケータイプロジェクト参加希望';
+      $_subject = _('ぶっとびケータイプロジェクト参加希望');
       break;
     case 'join-scp':
-      $_subject = 'Mozilla Summer Code Party in Kobe 参加予定';
+      $_subject = _('Mozilla Summer Code Party in Kobe 参加予定');
       break;
   }
 }
+
+// 本文
+$bodyparts = array(
+  _('お名前') . "\n" . $_POST['name'],
+  _('メールアドレス') . "\n" . $_POST['email'],
+  _('年齢') . "\n" . $_POST['age'],
+  _('学校名／所属') . "\n" . $_POST['organization']
+);
+if (!$_isEvent) {
+  $interest_table = array(
+    'think' => _('考える'),
+    'create' => _('創造する'),
+    'design' => _('デザインする'),
+    'make&build' => _('作る'),
+    'lead' => _('教える')
+  );
+  if ($_POST['interest']) {
+    $interest = implode(', ', array_map(function($v){
+        global $interest_table;
+        return $interest_table[$v];
+      }, $_POST['interest']));
+  }
+  else {
+    $interest = _('なし');
+  }
+  $bodyparts[] = _('興味のある体験') . "\n" . $interest;
+}
+$bodyparts[] = _('コメント') . "\n" . $_POST['comment'];
+$_body = implode("\n\n", $bodyparts);
 
 // スパム送信を防ぐためのキー
 $_key = md5(strtotime('today') * 5716);
@@ -74,53 +96,24 @@ if (isset($_POST['key']) && $_POST['key'] === $_key) {
   // 入力値のチェック
   if (empty($_POST['name'])) {
     $_status->code = -1;
-    $_status->text = 'お名前が入力されていません。';
+    $_status->text = _('お名前が入力されていません。');
   } else if (empty($_POST['email'])) {
     $_status->code = -1;
-    $_status->text = 'メールアドレスが入力されていません。';
+    $_status->text = _('メールアドレスが入力されていません。');
   } else if (!preg_match('/^[\w\.\-]+@[\w\.\-]+$/', $_POST['email'])) {
     $_status->code = -1;
-    $_status->text = 'メールアドレスの形式が正しくありません。';
+    $_status->text = _('メールアドレスの形式が正しくありません。');
   }
 
   if ($_status->code === 0) {
-    if (!$_isEvent) {
-      // 興味ある体験
-      $_interest_table = array(
-        'think' => '考える',
-        'create' => '創造する',
-        'design' => 'デザインする',
-        'make&build' => '作る',
-        'lead' => '教える'
-      );
-      if ($_POST['interest']) {
-        $_interest = implode(', ', array_map(function($v){
-            global $_interest_table;
-            return $_interest_table[$v];
-          }, $_POST['interest']));
-      }
-      else {
-        $_interest = 'なし';
-      }
-    }
-    $_body = array(
-      'お名前' . "\n" . $_POST['name'],
-      'メールアドレス' . "\n" . $_POST['email'],
-      '年齢' . "\n" . $_POST['age'],
-      '学校名／所属' . "\n" . $_POST['organization']
-    );
-    if (!$_isEvent) {
-      $_body[] = '興味のある体験' . "\n" . $_interest;
-    }
-    $_body[] = 'コメント' . "\n" . $_POST['comment'];
     $_fields = array(
       'key' => 'ohweiGeeXeichogie1aiLa9eejei)z7i',
       'from' => $_POST['email'],
-      'to' => ($_debug ? $_debug_mail : 'education@mozilla-japan.org'),
+      'to' => ($DEBUG ? $DEBUG_mail : 'education@mozilla-japan.org'),
       'cc' => '',
       'bcc' => '',
-      'subject' => $_subject . ' (Web サイトからの問い合わせ)',
-      'body' => implode("\n\n", $_body)
+      'subject' => $_subject . _(' (Web サイトからの問い合わせ)'),
+      'body' => $_body
     );
 
     // API 経由でメールを送信 (mozilla.jp のミラーサーバから直接送信できない場合を考慮)
@@ -134,10 +127,10 @@ if (isset($_POST['key']) && $_POST['key'] === $_key) {
 
     if (curl_getinfo($_ch, CURLINFO_HTTP_CODE) !== 200 || $_result === false || $_result === '-1') {
       $_status->code = -1;
-      $_status->text = 'フォームを送信できませんでした。申し訳ありませんが、また後でお試しください。';
+      $_status->text = _('フォームを送信できませんでした。申し訳ありませんが、また後でお試しください。');
     } else {
       $_status->code = 1;
-      $_status->text = 'ありがとうございました。折り返しご連絡を差し上げます。';
+      $_status->text = _('ありがとうございました。折り返しご連絡を差し上げます。');
     }
 
     curl_close($_ch);
