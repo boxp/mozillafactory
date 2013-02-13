bc6aa98d9ffe503644de4c569b03315b74ae3a9c
saneyuki_s <saneyuki.s.snyk@gmail.com>
2013-02-13 17:26:51 +0900

    Config scss env.

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+# syntax:
+preferred_syntax = :scss
+
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -7,6 +10,16 @@ sass_dir = "/"
 images_dir = "img"
 javascripts_dir = "js"
 
+if environment === :development then
+  output_style = :expanded
+  line_comments = true
+  sass_options = {:debug_info => false, :cache => true}
+else
+  output_style = :compact
+  line_comments = false
+  sass_options = {:debug_info => false, :cache => false}
+end
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
@@ -15,7 +28,6 @@ javascripts_dir = "js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
